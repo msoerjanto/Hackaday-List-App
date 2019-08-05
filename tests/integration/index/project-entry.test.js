ae8348c07c01ca266ae31dbe683597bb0ab10dfb
msoerjanto <michaelsoerjanto@gmail.com>
2019-08-04 18:09:50 -0700

    Added some integration tests using Nightwatch. Added pagination support Added and updated ids and class for integration testing in various templates

--- tests/integration/index/project-entry.test.js
+++ tests/integration/index/project-entry.test.js
@@ -1,8 +1,11 @@
 module.exports = {
-    'Project entry is loaded' : function (browser) {
+    'Project entry should load' : function (browser) {
         browser
             .url(browser.launch_url)
             .waitForElementVisible('.project-entry');
+    },
+    'User modal should not be rendered by default': function(browser) {
+        browser.expect.element('.user-modal').to.be.not.present;
     },
     after: function (browser) {
         browser.end();
